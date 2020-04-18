c508152a5b3655929f647821e75781b8c57021a1
Sebastian Scaini <sebastianscaini@gmail.com>
2020-04-18 18:03:07 -0400

    Fading Text Scene
    Made the visual elements of the text scene have a fading function.

--- Scripts/TextBox.gd
+++ Scripts/TextBox.gd
@@ -1,22 +1,18 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#These are for testing. Remove once implemented in the game loop.
+	set_story_text("Test")
+	handle_background_visibility(true)
 	
-	pass # Replace with function body.
+func handle_background_visibility(backgroundVisible):
+	if(backgroundVisible):
+		$Tween.interpolate_property($Background, "modulate", Color(1, 1, 1, 0), Color(1, 1, 1, 1), 1, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	else:
+		$Tween.interpolate_property($Background, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 1)
+		
+	$Tween.start()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
-	pass
-
-func setStoryText(storyText):
-	get_node("TextBox").bbcode_text = "[center]" + storyText + "[/center]"
-	pass
+func set_story_text(storyText):
+	$Background/TextBox.bbcode_text = "[center]" + storyText + "[/center]"
